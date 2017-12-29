590e98f6e5670c3b146e43e0c05dc51dba4cb24a
Abra.Zhang <zhangxiangyun@carkusoft.com>
2017-12-29 18:12:37 +0800

    open file

--- data_scan.inc.php
+++ data_scan.inc.php
@@ -3,4 +3,11 @@ if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
+$filePath = __DIR__ . "/files/evaluation_results.dat";
+$file = fopen($filePath, "r, ccs=UTF-8");
+// $contents = fread($file, filesize($filePath));
+if ($file) {
+	fclose($filePath);
+}
+
 include template('data_scan:index');
