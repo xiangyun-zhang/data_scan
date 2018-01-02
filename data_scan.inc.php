7c389fcde9e95b5dcb5e9de83917eb57366b246f
Abra.Zhang <zhangxiangyun@carkusoft.com>
2018-01-02 16:37:19 +0800

    get all level array

--- data_scan.inc.php
+++ data_scan.inc.php
@@ -2,12 +2,27 @@
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
+include "const.php";
 
-$filePath = __DIR__ . "/files/evaluation_results.dat";
-$file = fopen($filePath, "r, ccs=UTF-8");
-// $contents = fread($file, filesize($filePath));
-if ($file) {
-	fclose($filePath);
+foreach ($SUBJECT as $k => $v) {
+	$filePath = __DIR__ . "/files/" . $k . ".dat";
+	$file = fopen($filePath, "r, ccs=UTF-8");
+	$content = fread($file, filesize($filePath));
+	$content = explode(PHP_EOL, $content);
+	foreach ($content as $value) {
+		if (in_array($value, $LEVEL)) {
+			$tmp = $value;
+			$contents[$k][$tmp] = [];
+			continue;
+		}else {
+			$contents[$k][$tmp][] .= $value;
+		}
+	}
+
+	if ($file) {
+		fclose($filePath);
+	}
 }
+// var_dump($contents);
 
 include template('data_scan:index');
